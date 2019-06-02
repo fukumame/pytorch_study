e691ccec3d1000be9754861eeb67476d20cd48ff
fukumame <tanuma0115@gmail.com>
2019-06-02 17:46:48 +0900

    update codes

--- cifar10_demo.py
+++ cifar10_demo.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from tqdm import tqdm
 import torch.optim as optim
 import argparse
+import time
 
 class MyNet(nn.Module):
     def __init__(self):
@@ -26,7 +27,11 @@ class MyNet(nn.Module):
         x = self.fc3(x)
         return x
 
-def get_loader(batch_size):
+def get_loader(batch_size, workers):
+
+    print(f"batch_size: {batch_size}")
+    print(f"workers: {workers}")
+
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -35,22 +40,22 @@ def get_loader(batch_size):
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=2)
+                                              shuffle=True, num_workers=workers)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                             shuffle=False, num_workers=2)
+                                             shuffle=False, num_workers=workers)
 
     return trainloader, testloader
 
-def validate_test_acc(test_loader, model):
+def validate_test_acc(test_loader, model, device):
     correct = 0
     total = 0
 
     with torch.no_grad():
         for data in test_loader:
-            images, labels = data
+            images, labels = data[0].to(device), data[1].to(device)
             outputs = model(images)
             _, predicted = torch.max(outputs.data, dim=1)
             total += labels.size(0)
@@ -63,20 +68,23 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batchsize', '-b', type=int, default=32)
     parser.add_argument('--epoch', '-e', type=int, default=100)
+    parser.add_argument('--workers', '-w', type=int, default=2)
     args = parser.parse_args()
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     model = MyNet()
-    model.to(device)
 
-    if torch.cuda.is_available():
+    if torch.cuda.device_count() > 1:
+        print("Use", torch.cuda.device_count(), "GPUs")
         model = nn.DataParallel(model)
+        model.to(device)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    train_loader, test_loader = get_loader(args.batchsize)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    train_loader, test_loader = get_loader(args.batchsize, args.workers)
 
+    start = time.time()
     for epoch in tqdm(range(args.epoch)):
         running_loss = 0.0
         for i, data in enumerate(train_loader):
@@ -90,9 +98,11 @@ def main():
 
             running_loss += loss.item()
 
-        val_acc = validate_test_acc(test_loader, model)
+        val_acc = validate_test_acc(test_loader, model, device)
         ave_loss = running_loss/len(train_loader)
         print('epoch: {}, loss: {:.4f}, val_acc: {:.4f}'.format(epoch + 1, ave_loss, val_acc))
+    elapsed_time = time.time() - start
+    print(f"elapsed_time: {elapsed_time}")
 
 if __name__ == "__main__":
     main()
